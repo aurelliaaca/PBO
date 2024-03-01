e5469e89d84905a4d3015f08a7ceda806abaa780
aurelliaaca <114278824+aurelliaaca@users.noreply.github.com>
2024-03-01 20:07:32 +0700

    Add files via upload

---
+++ pbo 2/titik dan garis/MGaris.java
@@ -0,0 +1,31 @@
+public class MGaris{
+    public static void main(String[] args) {
+        Titik t1;
+        Titik t2;
+        Garis g1;
+
+        t1 = new Titik();
+        t2 = new Titik();
+        g1 = new Garis(t1,t2);
+
+        t1.setAbsis(1);
+        t1.setOrdinat(2);
+        t2.setAbsis(3);
+        t2.setOrdinat(4);
+
+        double counterGaris = Garis.getCounterGaris();
+        System.out.println("jumlah objek garis : " + counterGaris);
+
+        System.out.println("Panjang garis g1 : " + g1.getPanjang());
+
+        System.out.println("Gradien g1 : " + g1.getGradien());
+        
+        Garis g2 = g1.getRefleksiY();
+        Titik awalRefleksi = g2.getTitikAwal();
+        Titik akhirRefleksi = g2.getTitikAkhir();
+        System.out.println("Refleksi Y : awal g2(" + awalRefleksi.getAbsis() +","+ awalRefleksi.getOrdinat()+")");
+        System.out.println("Refleksi Y : akhir g2(" + akhirRefleksi.getAbsis() +","+ akhirRefleksi.getOrdinat()+")");
+
+        System.out.println("Is tegak lurus : " + isTegakLurus(g2));
+    }
+}
