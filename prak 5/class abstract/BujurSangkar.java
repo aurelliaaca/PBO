31345626399aab405aef303e41bde2ff2b3cad23
aurelliaaca <114278824+aurelliaaca@users.noreply.github.com>
2024-03-20 10:59:33 +0700

    Delete prak 5 directory

--- prak 5/class abstract/BujurSangkar.java
+++
@@ -1,8 +0,0 @@
-public class BujurSangkar extends BangunDatar {
-    public double hitungLuas(double sisi){
-        luas = sisi * sisi;
-        return luas;
-    } 
-    // Jika BangunDatar tidak diimplementasikan maka method hitungLuas tidak dapat dijalankan
-    // karena method hitungLuas mewarisi luas dari class abstract BangunDatar
-}
